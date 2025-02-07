5d1a95302f6a870d4523fbd3df27af8e9edb26b5
andriferry <ferryandri35@gmail.com>
2025-02-07 20:22:31 +0700

    First concept eslint

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
+<script setup lang="ts">
+interface NavData {
+  heading?: string
+  title?: string
+  to?: string
+  icon?: string
+  childrem?: any[]
+}
+
+const hello = () => {
+  const dataHer = ''
+}
+</script>
+
 <template>
-  <div class="bg-red-500">
+        <div class="bg-red-500">
     Hello
   </div>
 </template>
