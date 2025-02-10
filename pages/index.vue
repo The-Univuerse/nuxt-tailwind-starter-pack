4f86d0e02849cd67fad50bd146394ecf91c14103
andriferry <ferryandri35@gmail.com>
2025-02-10 19:23:09 +0700

    Update logo flex

--- pages/index.vue
+++ pages/index.vue
@@ -35,22 +35,24 @@ const featuresList = computed(() => {
         Welcome to the Universe
       </h1>
 
-      <p class="text-secondary mt-2 text-lg text-center font-medium">
+      <p class="text-secondary mt-2 text-xl text-center font-medium">
         Save your crutial time with our universe
       </p>
 
       <div class="flex gap-5 mt-10 items-center justify-center">
-        <img class="w-40" src="/assets/icon/nuxt-dark.png" alt="" />
-        <Icon name="tabler:plus" class="text-4xl text-secondary self-end" />
-        <img class="w-52 self-center" src="/assets/icon/tailwindcss-dark.svg" alt="" />
+        <img class="w-32 sm:w-36" src="/assets/icon/nuxt-dark.png" alt="" />
+        <Icon name="tabler:plus" class="text-3xl text-secondary self-end" />
+        <img class="w-32 sm:w-40 self-center" src="/assets/icon/tailwindcss-dark.svg" alt="" />
       </div>
 
-      <div class="flex gap-5 mt-16 items-center justify-center">
-        <Icon
-          v-for="(data, index) in featuresList"
-          :key="index"
-          :name="data.icon" class="text-3xl"
-        />
+      <div class="flex justify-center">
+        <div class="flex w-[250px] sm:w-full gap-5 mt-16 items-center flex-wrap justify-center">
+          <Icon
+            v-for="(data, index) in featuresList"
+            :key="index"
+            :name="data.icon" class="text-3xl"
+          />
+        </div>
       </div>
     </div>
   </div>
